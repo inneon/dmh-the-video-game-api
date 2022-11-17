2d6d149cfb29454801f6eac6e544f19b248899c7
jonny <jonathon.wright@converge.io>
2022-11-17 14:12:01 +0000

    feat: add queries and dispatcher

--- src/application/errors/index.ts
+++ src/application/errors/index.ts
@@ -1,2 +1,3 @@
 export { default as DuplicateGameNameError } from "./duplicateGameNameError"
 export { default as PlayerNotFoundError } from "./playerNotFoundError"
+export { default as HandlerNotFoundError } from "./handlerNotFoundError"
